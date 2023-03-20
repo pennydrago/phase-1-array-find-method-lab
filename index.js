1f4fdd817d5687df1cde6190febfd80276feb0ac
Penelope Drago <prd@drago.com.au>
2023-03-21 09:50:16 +1100

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function evaluateRecord(item) {
+  return item.result === "W";
+}
+
+function superbowlWin(records) {
+  const winResult = records.find(evaluateRecord);
+  if (winResult !== undefined) {
+    return winResult.year;
+  }
+}
